3d21e2a4618302d79f5e8432fe9bac95b2f749c1
Alban Mouton <alban.mouton@gmail.com>
2014-11-11 21:01:11 +0100

    [deploy-demos] more advanced support of image proportions

--- src/directives/fact-simple-slideshow/fact-simple-slideshow.js
+++ src/directives/fact-simple-slideshow/fact-simple-slideshow.js
@@ -1,6 +1,6 @@
 angular.module('fact-client.directives.simple-slideshow', ['ngAnimate'])
-	.directive('factSimpleSlideshow', ['$timeout', 'FACT_API_URL',
-		function($timeout, FACT_API_URL) {
+	.directive('factSimpleSlideshow', ['$timeout', '$log', 'FACT_API_URL',
+		function($timeout, $log, FACT_API_URL) {
 			return {
 				restrict: 'EA',
 				templateUrl: 'directives/fact-simple-slideshow/fact-simple-slideshow.html',
@@ -9,14 +9,17 @@ angular.module('fact-client.directives.simple-slideshow', ['ngAnimate'])
 					preloadDelay: '=preloadDelay',
 					slide: '=',
 					rotate: '=',
-					fade: '='
+					fade: '=',
+					truncate: '='
 				},
-				link: function(scope) {
+				link: function(scope, element) {
 					scope.baseUrl = FACT_API_URL;
 					// preload delay is 0 by default
 					var preloadDelay = scope.preloadDelay === undefined ? 0 : scope.preloadDelay;
 
 					scope.iter = 0;
+					scope.imgStyles = [{}, {}];
+					
 					scope.$watch('fact', function(newVal, oldVal) {
 						// Set nextFact to allow browser caching of depiction
 						scope.nextFact = newVal;
@@ -24,6 +27,36 @@ angular.module('fact-client.directives.simple-slideshow', ['ngAnimate'])
 							scope.previousFact = oldVal;
 							scope.currentFact = newVal;
 							scope.iter += 1;
+
+							if (scope.currentFact) {
+
+								// process image dimension base on multiples criterias
+
+								// first the size of the image that will be loaded
+								// weird, it seems that width and height are mixed up by the API ?
+								var originalWidth = scope.currentFact.depiction.width;
+								var originalHeight = scope.currentFact.depiction.height;
+								// also the size of the container element
+								var container = element.parent()[0];
+								var containerWidth = container.offsetWidth;
+								var containerHeight = container.offsetHeight;
+								// then a horizontal/vertival ratio that indicates if the image
+								// has a more horizontal shape than the container (<1) or more vertical (>1)
+								var hvRatio = (containerWidth / containerHeight) / (originalWidth / originalHeight);
+
+								var currentStyle = scope.imgStyles[scope.iter % 2] = {};
+								if ((hvRatio > 1 && scope.truncate) || (hvRatio < 1 && !scope.truncate)) {
+									currentStyle.width = containerWidth + 'px';
+									var height = originalHeight * (containerWidth / originalWidth);
+									currentStyle.height = height + 'px';
+									currentStyle['margin-top'] = ((containerHeight - height) / 2) + 'px';
+								} else {
+									currentStyle.height = containerHeight + 'px';
+									var width = originalWidth * (containerHeight / originalHeight);
+									currentStyle.width = width + 'px';
+									currentStyle['margin-left'] = ((containerWidth - width) / 2) + 'px';
+								}
+							}
 						}, preloadDelay);
 					});
 				}
--- test/data/facts.js
+++ test/data/facts.js
@@ -2,23 +2,80 @@ var testData = angular.module('test.data.facts', []);
 
 testData.constant('FACTS', [{
 	"factType": "53c98170e9b0a9fd36f3e4ff",
-	"phrase": "Ryan Jones was born in 1984",
-	"question": "When Ryan Jones was born ?",
-	"response": "1984-06-13T22:00:00.000Z",
-	"_id": "53c9906ce9b0a9fd3601c80c",
-	"depiction": "/test/data/verySmall_Ryan_Jones_Oilers_2012.jpg"
+	"phrase": "Sergey Shustikov was born in 1989",
+	"question": "When Sergey Shustikov was born ?",
+	"response": "1989-03-04T23:00:00.000Z",
+	"_id": "53c9939de9b0a9fd360474f4",
+	"depiction": {
+		"_id": "5414837cd43edee36edaba4c",
+		"downloadDate": "2014-09-13T17:48:44.232Z",
+		"originURL": "http://upload.wikimedia.org/wikipedia/commons/b/ba/Shustikov40.JPG",
+		"sizes": [{
+			"key": "verySmall",
+			"cachePath": "/f4/ed/dd0b/c3eee3aa711e974db9c6f9f7/verySmall_Shustikov40.JPG",
+			"_id": "5414837cd43edee36edaba4c"
+		}],
+		"brokenLink": false,
+		"key": "verySmall",
+		"cachePath": "/f4/ed/dd0b/c3eee3aa711e974db9c6f9f7/verySmall_Shustikov40.JPG",
+		"url": "/test/data/verySmall_LubchencoJane-NOAA.jpg",
+		"width": 326,
+		"height": 490
+	}
 }, {
 	"factType": "53c98170e9b0a9fd36f3e4ff",
-	"phrase": "Jane Lubchenco was born in 1947",
-	"question": "When Jane Lubchenco was born ?",
-	"response": "1947-12-03T23:00:00.000Z",
-	"_id": "53c98b3ae9b0a9fd36fd44f1",
-	"depiction": "/test/data/verySmall_LubchencoJane-NOAA.jpg"
+	"phrase": "Zachary Richard was born in 1950",
+	"question": "When Zachary Richard was born ?",
+	"response": "1950-09-07T22:00:00.000Z",
+	"_id": "53c98373e9b0a9fd36f5ea3f",
+	"depiction": {
+		"_id": "540d9461d43edee36ed9173f",
+		"downloadDate": "2014-09-08T11:34:57.903Z",
+		"originURL": "http://upload.wikimedia.org/wikipedia/commons/b/b1/Zachary_Richard.jpg",
+		"sizes": [{
+			"key": "verySmall",
+			"cachePath": "/00/a6/e0cf/b65cddef10d6bf1bae7a59ab/verySmall_Zachary_Richard.jpg",
+			"_id": "540d9461d43edee36ed9173f"
+		}],
+		"brokenLink": false,
+		"key": "verySmall",
+		"cachePath": "/00/a6/e0cf/b65cddef10d6bf1bae7a59ab/verySmall_Zachary_Richard.jpg",
+		"url": "/test/data/verySmall_John_S._Clarke.jpg",
+		"width": 331,
+		"height": 483
+	}
 }, {
 	"factType": "53c98170e9b0a9fd36f3e4ff",
-	"phrase": "John Sleeper Clarke was born in 1833",
-	"question": "When John Sleeper Clarke was born ?",
-	"response": "1833-09-02T22:00:00.000Z",
-	"_id": "53c99433e9b0a9fd3604e4c2",
-	"depiction": "/test/data/verySmall_John_S._Clarke.jpg"
+	"phrase": "Tim Hill was born in 1936",
+	"question": "When Tim Hill was born ?",
+	"response": "1936-08-22T22:00:00.000Z",
+	"_id": "53c9881ce9b0a9fd36fa70ab",
+	"depiction": {
+		"_id": "540db401d43edee36ed91fe3",
+		"downloadDate": "2014-09-08T13:49:53.546Z",
+		"originURL": "http://upload.wikimedia.org/wikipedia/commons/a/a2/KCE_Tim_Hill.jpg",
+		"sizes": [{
+			"key": "veryLarge",
+			"cachePath": "/67/23/e4cb/ecd0d369f60244de2b4328af/veryLarge_KCE_Tim_Hill.jpg",
+			"_id": "540db401d43edee36ed91fe6"
+		}, {
+			"key": "large",
+			"cachePath": "/67/23/e4cb/ecd0d369f60244de2b4328af/large_KCE_Tim_Hill.jpg",
+			"_id": "540db401d43edee36ed91fe5"
+		}, {
+			"key": "small",
+			"cachePath": "/67/23/e4cb/ecd0d369f60244de2b4328af/small_KCE_Tim_Hill.jpg",
+			"_id": "540db401d43edee36ed91fe4"
+		}, {
+			"key": "verySmall",
+			"cachePath": "/67/23/e4cb/ecd0d369f60244de2b4328af/verySmall_KCE_Tim_Hill.jpg",
+			"_id": "540db401d43edee36ed91fe3"
+		}],
+		"brokenLink": false,
+		"key": "verySmall",
+		"cachePath": "/67/23/e4cb/ecd0d369f60244de2b4328af/verySmall_KCE_Tim_Hill.jpg",
+		"url": "/test/data/verySmall_Ryan_Jones_Oilers_2012.jpg",
+		"width": 408,
+		"height": 391
+	}
 }]);
